26e127656db113238e324cac19d8c258d4f3cde5
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-06-19 19:23:11 +0200

    working on css hp

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
+  get 'about', to: 'pages#about'
+
   resources :appointments, only: [:new]
 
-  resources :patients, only: [:show]
-
-  get 'about', to: 'pages#about'
+  resources :users, only: [:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
