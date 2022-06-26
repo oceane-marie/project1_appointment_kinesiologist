b104920d32c79d6f58cf39291444989843c4a2c5
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-06-26 14:01:59 +0200

    relaunching

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,8 +6,8 @@ class UserMailer < ApplicationMailer
   #
   def appointment
     @user = params[:user] # Instance variable => available in view
-    # @appointment.date = params[:date]
-    # @appointment.time = params[:time]
+    # @date = appointment.date
+    # @time = appointment.time
     mail(to: @user.email, subject: 'Votre rendez-vous a bien été confirmé')
     # This will render a view in `app/views/user_mailer`!
   end
