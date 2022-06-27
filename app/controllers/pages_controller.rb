33f506847d6af03b9bf311ffc61f20262c1bc7da
Oceane Feltrin <oceanefeltrin@yahoo.fr>
2022-06-27 16:12:16 +0200

    perso devise views

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home, raise: false
+  before_action :authenticate_user!, only: :home, raise: false
 
   def home
   end
